da2382e26c3d88383af79eb88c0043add2ba5e1a
Juan Pablo Jimenez <jchantej@gmail.com>
2017-09-28 23:33:15 +0200

    Se agregaron atributos  a las clases Book y Author, asi como relacion de agregacion entre ellas

--- src/main/java/Patrones/Author.java
+++ src/main/java/Patrones/Author.java
@@ -2,4 +2,48 @@ package Patrones;
 
 public class Author {
 
+    private long id;
+
+    private String name;
+
+    private String language;
+
+    public Author(Long id, String name, String language) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.language = language;
+    }
+    
+    public Author(Long id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
 }
--- src/main/java/Patrones/Book.java
+++ src/main/java/Patrones/Book.java
@@ -1,5 +1,82 @@
 package Patrones;
 
+import java.util.Calendar;
+
 public class Book {
+
+    private long id;
+
+    private long isbn;
+
+    private String title;
+
+    private Calendar date;
+
+    private Author autor;
+
+    public Book(long id, long isbn, String title, Calendar date, Author autor) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.date = date;
+        this.autor = autor;
+    }
+
+    public Book(long id, long isbn, String title, Calendar date) {
+        super();
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.date = date;
+    }
+
+    public Book(long id, long isbn, String title) {
+        super();
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(long isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public Author getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Author autor) {
+        this.autor = autor;
+    }
+    
+    
 
 }
