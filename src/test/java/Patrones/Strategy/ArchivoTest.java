e553802ad5af5df66137edc3957e1a1ac280e55d
Juan Pablo Jimenez <jchantej@gmail.com>
2017-10-06 13:09:56 +0200

    Refactorig Test

--- src/test/java/Patrones/Strategy/ArchivoTest.java
+++ src/test/java/Patrones/Strategy/ArchivoTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class ArchivoTest {
 
     private enum tipoArchivo {
-        VIDEO, AUDIO, NOT_SOPORTED
+        VIDEO, AUDIO, IMAGEN, NOT_SOPORTED
     };
 
     private Archivo archivo;
@@ -16,19 +16,18 @@ public class ArchivoTest {
     public void compresionSegunTipoArchivoTest() {
 
         if ("VIDEO".equals(tipoArchivo.VIDEO.toString())) {
-
             archivo = new Archivo(new CompresionVideo("video"));
-
             assertEquals("video.7Zip", archivo.comprimirArchivo());
         }
-
         if ("AUDIO".equals(tipoArchivo.AUDIO.toString())) {
-
             archivo = new Archivo(new CompresionAudio("audio"));
-
             assertEquals("audio.WinRar", archivo.comprimirArchivo());
+      
         }
-
+        if ("IMAGEN".equals(tipoArchivo.IMAGEN.toString())) {
+            archivo = new Archivo(new CompresionImagen("imagen"));
+            assertEquals("imagen.Zip", archivo.comprimirArchivo());
+        }
         if (!"OTRO".equals(tipoArchivo.NOT_SOPORTED.toString())) {
 
             archivo = new Archivo(new CompresionNoSoportada("nombre"));
@@ -41,7 +40,6 @@ public class ArchivoTest {
     @Test
     public void compresionVideoTest() {
         Archivo archivo = new Archivo(new CompresionVideo("video"));
-        
         assertEquals("video.7Zip", archivo.comprimirArchivo());
 
     }
@@ -52,12 +50,18 @@ public class ArchivoTest {
         assertEquals("audio.WinRar", archivo.comprimirArchivo());
 
     }
+    
+    @Test
+    public void compresionImagenTest() {
+        archivo = new Archivo(new CompresionImagen("imagen"));
+        assertEquals("imagen.Zip", archivo.comprimirArchivo());
+
+    }
 
     @Test
     public void compresionNoSoportadaTest() {
         Archivo archivo = new Archivo(new CompresionNoSoportada("nombre"));
         assertEquals("NO SOPORTADA", archivo.comprimirArchivo());
-
     }
 
 }
